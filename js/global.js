177f2b0b94f5f02eeea6f0662d6a7ef24e9f7267
MBorisova <75022961+mavbo@users.noreply.github.com>
2021-05-09 17:43:07 +0500

    Add files via upload

--- js/global.js
+++ js/global.js
@@ -1,1 +1,9 @@
 
+var d = new Date();
+
+var month=new Array("января","февраля","марта","апреля","мая","июня",
+"июля","августа","сентября","октября","ноября","декабря");
+
+
+document.getElementById('date').value = (d.getDate()+ " " + month[d.getMonth()]
++ " " + d.getFullYear() + " г.");
